6e4eb4fbf45dda346c46ccea5a764b25e5f8add2
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2024-07-02 20:45:04 +0100

    made changes to the server errorss

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const axios = require("axios");
-const dotenv = require("dotenv");
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
@@ -13,21 +14,27 @@ app.get("/api/hello", async (req, res) => {
   }
   const OPENWEATHERMAP_API_KEY = process.env.API_WEB_KEY;
   try {
-    const ipResponse = await axios.get(
-      "https://get.geojs.io/v1/ip/country.json"
-    );
+    const ipResponse = await axios.get("https://get.geojs.io/v1/ip/geo.json");
 
-    const CITY_NAME = ipResponse.data.name;
+    if (!ipResponse.data.city) {
+      throw new Error("City not found in IP response");
+    }
+
+    const CITY_NAME = ipResponse.data.city;
     const weatherResponse = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${OPENWEATHERMAP_API_KEY}&units=metric`
     );
 
-    const temperature = weatherResponse.data.main.temp;
+    if (!weatherResponse.data.main || !weatherResponse.data.main.temp) {
+      throw new Error("Temperature not found in weather response");
+    }
 
-    const greeting = `Hello, ${visitorName}, The temperature is ${temperature} degrees Celsius in ${ipResponse.data.name}`;
+    const temperature = weatherResponse.data.main.temp;
+    const greeting = `Hello, ${visitorName}, The temperature is ${temperature} degrees Celsius in ${CITY_NAME}.`;
+
     res.json({
       client_ip: ipResponse.data.ip,
-      location: ipResponse.data.name,
+      location: CITY_NAME,
       temperature,
       greeting,
     });
