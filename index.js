d33abfbd005579f43c3da5160fe581ba86132072
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2024-07-02 12:15:59 +0100

    Addded the open weather api to get the temperature

--- index.js
+++ index.js
@@ -4,18 +4,27 @@ const app = express();
 
 app.get("/api/hello", async (req, res) => {
   const visitorName = req.query.visitor_name;
-  const temperature = 11;
   if (!visitorName || visitorName.trim() === "") {
     return res.status(400).json({ message: "Visitor Name is Required" });
   }
+  const OPENWEATHERMAP_API_KEY = "abd3a67c4c331ef2c764830dfd53a59b";
   try {
     const ipResponse = await axios.get(
       "https://get.geojs.io/v1/ip/country.json"
     );
+
+    const CITY_NAME = ipResponse.data.name;
+    const weatherResponse = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${OPENWEATHERMAP_API_KEY}&units=metric`
+    );
+
+    const temperature = weatherResponse.data.main.temp;
+
     const greeting = `Hello, ${visitorName}, The temperature is ${temperature} degrees Celsius in ${ipResponse.data.name}`;
     res.json({
       client_ip: ipResponse.data.ip,
       location: ipResponse.data.name,
+      temperature,
       greeting,
     });
   } catch (error) {
@@ -23,6 +32,6 @@ app.get("/api/hello", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.listen(8000, () => {
   console.log("Server listening on port 3000");
 });
